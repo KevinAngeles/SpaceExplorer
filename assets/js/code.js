742ec2adf28b41be30ab59ef56572705e44bec40
KevinAngeles <kevin.angeles@outlook.com>
2016-09-15 20:36:07 -0500

    html corrected

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,5 +1,4 @@
 $(document).on("ready", function() {
-
 	//AJAX Call for Picture of the day
 	$.ajax({
 		url: "https://api.nasa.gov/planetary/apod?api_key=qVFWydcClpA2utQfaZBW0s0R70S0XQvDyh59Y2Jh",
@@ -141,8 +140,6 @@ $(document).on("ready", function() {
 		}).fail(function(err) {
 			console.log(err);
 		});
-
-
 	});
 
 	/* Function Update Cameras */
@@ -165,4 +162,3 @@ $(document).on("ready", function() {
 	}
 	/* End Update Cameras */
 });
-
