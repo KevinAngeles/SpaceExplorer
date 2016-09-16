ac2fbf3a5b501ff36966a493be68b81b04b4f4bf
Drew Wagner <Drew@wireless-10-147-71-255.public.utexas.edu>
2016-09-15 20:46:22 -0500

    updated asteroids js

--- assets/js/asteroids.js
+++ assets/js/asteroids.js
@@ -5,6 +5,10 @@ var startUrl = "https://api.nasa.gov/neo/rest/v1/feed?start_date=";
 var endUrl = "&end_date=";
 var finalUrl = "&api_key=qVFWydcClpA2utQfaZBW0s0R70S0XQvDyh59Y2Jh";
 
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 
 $("#send").on('click', function() {
   var start = $("#start").val().trim();
@@ -13,33 +17,43 @@ $("#send").on('click', function() {
 
 
 $.ajax({
-      url: nasaUrl,
-      method: "GET"
+	  url: nasaUrl,
+	  method: "GET"
 
-    }).done(function(res){
-      console.log(res);
-      var asteroids = res.near_earth_objects;
-      console.log(asteroids);
-      for (var key in asteroids) {
-        console.log("YEA");
-          if (asteroids.hasOwnProperty(start)) {
-              console.log(key + " -> " + asteroids[key]);
-              var nEobs = asteroids[key];
-          }
-        }
-      var proximity = 1000000000000000000;
-      console.log(nEobs);
-      for (i = 0; i < nEobs.length; i++) {
-        if (nEobs[i].close_approach_data[0].miss_distance.miles < proximity) {
-          proximity = nEobs[i].close_approach_data[0].miss_distance.miles;
-          var diameter = nEobs[i].estimated_diameter.feet.estimated_diameter_max;
-          var velocity = nEobs[i].close_approach_data[0].relative_velocity.miles_per_hour;
-        }
-      }
-      console.log(proximity, Math.round(diameter), Math.round(velocity));
-      var output = "<br><br>On " + start + " the closest asteroid was " + proximity + " miles away moving at a velocity of " + Math.round(velocity) + " miles/hour with an approximate maximum diameter of " + Math.round(diameter) + " feet.";
+	}).done(function(res){
+	  console.log(res);
+	  var asteroids = res.near_earth_objects;
+	  console.log(asteroids);
+	  for (var key in asteroids) {
+		  if (asteroids.hasOwnProperty(start)) {
+			  console.log(key + " -> " + asteroids[key]);
+			  var nEobs = asteroids[key];
+		  }
+		}
+	  var proximity = 1000000000000000000;
+	  console.log(nEobs);
+	  for (i = 0; i < nEobs.length; i++) {
+		if (nEobs[i].close_approach_data[0].miss_distance.miles < proximity) {
+		  proximity = nEobs[i].close_approach_data[0].miss_distance.miles;
+		  var diameter = nEobs[i].estimated_diameter.feet.estimated_diameter_max;
+		  var velocity = nEobs[i].close_approach_data[0].relative_velocity.miles_per_hour;
+		  var neoLink = nEobs[i].nasa_jpl_url;
+		  var neoName = nEobs[i].name;
+		}
 
-      $(".go").append(output);
+	  }
+	  // console.log(proximity, Math.round(diameter), Math.round(velocity));
+	  console.log(velocity);
+	  var year = start.slice(0,4);
+	  var date = start.slice(5,start.length);
+	  var newDate = date + "-" + year;
+	  var output = "<br><br>On " + newDate + " the nearest asteroid, NASA ID: " + neoName + ", missed Earth by " 
+	  + numberWithCommas(proximity) + " miles, was moving at a velocity of " + numberWithCommas(Math.round(velocity)) 
+	  + " miles/hour and had an approximate maximum diameter of " + numberWithCommas(Math.round(diameter)) + " feet.";
+
+	  var moreInfo = "<br><div><a href=" +neoLink + ">Would you like to know more?</a></div>";
+	  $(".go").append(output);
+	  $(".go").append(moreInfo);
   });
 
 });
--- assets/js/code.js
+++ assets/js/code.js
@@ -1,6 +1,10 @@
 
 $(document).on("ready", function() {
 
+    $( function() {
+    $( "#datepicker" ).datepicker();
+  } );
+
     //AJAX Call for Picture of the day
     $.ajax({
         url: "https://api.nasa.gov/planetary/apod?api_key=qVFWydcClpA2utQfaZBW0s0R70S0XQvDyh59Y2Jh",
